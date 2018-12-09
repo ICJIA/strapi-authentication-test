a4efd9ab8378dddd47b4a19427ce5e3a57ce6354
Chris Schweda <cschweda@gmail.com>
2018-12-09 14:24:45 -0600

    update: add fade page transition

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,9 @@
     </v-toolbar>
 
     <v-content>
-      <router-view/>
+      <transition name="fade" mode="out-in">
+        <router-view/>
+      </transition>
     </v-content>
   </v-app>
 </template>
@@ -63,5 +65,16 @@ export default {
 a {
   color: #555 !important;
   text-decoration: none;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
